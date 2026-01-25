4e454fc7af6a67e4ce01877236bf677f360cdd97
justBri <Bri@justBri.tech>
2026-01-25 12:33:56 -0800

    remove qbittorrent from docker cleanup, might cause issues

--- build_and_start.sh
+++ build_and_start.sh
@@ -11,7 +11,6 @@ docker compose down
 # Nuke old image, build new one
 docker rmi arrgo-arrgo 2>/dev/null || true
 docker rmi arrgo-indexer 2>/dev/null || true
-docker rmi arrgo-qbittorrent 2>/dev/null || true
 docker compose build --no-cache
 
 # Start the services
